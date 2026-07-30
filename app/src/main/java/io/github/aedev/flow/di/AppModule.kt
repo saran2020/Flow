4a9e94aef40c925af9f754541c280b5f2ace66dc
Saran <saran.sankaran@gmail.com>
2026-07-31 00:59:59 +0530

    refactor(di): remove redundant context provider from AppModule

--- app/src/main/java/io/github/aedev/flow/di/AppModule.kt
+++ app/src/main/java/io/github/aedev/flow/di/AppModule.kt
@@ -22,12 +22,6 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideContext(@ApplicationContext context: Context): Context {
-        return context
-    }
-
-    @Provides
-    @Singleton
     fun provideYouTube(): YouTube {
         return YouTube
     }
